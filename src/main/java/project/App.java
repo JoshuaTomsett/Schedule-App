63cf80b5677faf3add5390e4820ab0ce1a95acbb
Josh <josh.james.tomsett@gmail.com>
2023-02-11 12:33:31 +0000

    fixed button styling

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -34,5 +34,3 @@ public class App extends Application {
     }
 
 }
-
-// github test
--- src/main/java/project/PrimaryController.java
+++ src/main/java/project/PrimaryController.java
@@ -29,12 +29,38 @@ public class PrimaryController {
 
         if (actionEvent.getSource() == btnSchedule) {
             paneSchedule.toFront(); // set pane to visable (in front of the others)
+            btnSchedule.getStyleClass().clear(); // remove current styles
+            btnSchedule.getStyleClass().add("buttonActive"); // button active class
+
+            btnToDo.getStyleClass().clear();
+            btnToDo.getStyleClass().add("button");
+
+            btnGoals.getStyleClass().clear();
+            btnGoals.getStyleClass().add("button");
         }
+
         else if (actionEvent.getSource() == btnToDo) {
             paneToDo.toFront();
+            btnToDo.getStyleClass().add("buttonActive");
+            btnToDo.getStyleClass().remove("button");
+
+            btnSchedule.getStyleClass().clear();
+            btnSchedule.getStyleClass().add("button");
+
+            btnGoals.getStyleClass().clear();
+            btnGoals.getStyleClass().add("button");
         }
+
         else if (actionEvent.getSource() == btnGoals) {
             paneGoals.toFront();
+            btnGoals.getStyleClass().add("buttonActive");
+            btnGoals.getStyleClass().remove("button");
+
+            btnToDo.getStyleClass().clear();
+            btnToDo.getStyleClass().add("button");
+
+            btnSchedule.getStyleClass().clear();
+            btnSchedule.getStyleClass().add("button");
         }
     }
 }
